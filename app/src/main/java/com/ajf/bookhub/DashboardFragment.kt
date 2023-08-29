ce3bea70fb004a5e348988bc519b708cfecc8590
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-30 00:51:08 +0400

    > Adding a Recycler View.

--- app/src/main/java/com/ajf/bookhub/DashboardFragment.kt
+++ app/src/main/java/com/ajf/bookhub/DashboardFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var rvDashboard: RecyclerView
+    private lateinit var layoutManager: RecyclerView.LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
+
+        rvDashboard = view.findViewById(R.id.rvDashboard)
+        layoutManager = LinearLayoutManager(activity)
+
+        return view
     }
 }
---
+++ app/src/main/java/com/ajf/bookhub/DashboardRecyclerAdapter.kt
@@ -0,0 +1,5 @@
+package com.ajf.bookhub
+
+class DashboardRecyclerAdapter {
+
+}
