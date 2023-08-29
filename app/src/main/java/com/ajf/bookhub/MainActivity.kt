cc067da55ba6bdb78038f08596acdd3a5d7e1cd6
Shah Arbaaz Jaan <arbaazaj1@gmail.com>
2023-08-29 17:18:44 +0400

    > Added functionality to navigation drawer items.

--- app/src/main/java/com/ajf/bookhub/MainActivity.kt
+++ app/src/main/java/com/ajf/bookhub/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ajf.bookhub
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
@@ -40,6 +41,33 @@ class MainActivity : AppCompatActivity() {
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.dashboard -> {
+                    Toast.makeText(this@MainActivity, "Clicked on Dashboard", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                R.id.favorite -> {
+                    Toast.makeText(this@MainActivity, "Clicked on Favorite", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                R.id.profile -> {
+                    Toast.makeText(this@MainActivity, "Clicked on Profile", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                R.id.about -> {
+                    Toast.makeText(this@MainActivity, "Clicked on About Us", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+
+            return@setNavigationItemSelectedListener true
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
